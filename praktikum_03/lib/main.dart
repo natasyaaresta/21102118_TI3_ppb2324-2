e162e84b383a4bc51e6cd354d71b5bf0f9d6badc
natasya aresta <putrianatasyaa09@gmail.com>
2024-03-15 10:59:50 +0700

    praktikum_03

--- praktikum_03/lib/main.dart
+++ praktikum_03/lib/main.dart
@@ -7,23 +7,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi Pertemuan 3'),
-        ),
-        body: Container(
-          margin: EdgeInsets.only(top: 100, left: 50),
-          color: Colors.amber,
-          child: Text(
-            "Hello World!",
-            style: TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 50, 
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
