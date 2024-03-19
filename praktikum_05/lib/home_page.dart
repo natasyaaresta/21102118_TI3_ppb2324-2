d36ac5f242f1440098c3367a725529d491749367
natasya aresta <putrianatasyaa09@gmail.com>
2024-03-19 13:15:05 +0700

    praktikum_05

--- praktikum_05/lib/home_page.dart
+++ praktikum_05/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praktikum_05/second_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,7 +11,14 @@ class HomePage extends StatelessWidget {
       ),
       body: Center (
         child: ElevatedButton (
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SecondPage ()
+              ),
+            );
+          },
           child: const Text('Menuju halaman kedua'),
         ),
       ),
