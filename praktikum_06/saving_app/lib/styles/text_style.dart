f01c679ae531518a40d558bdee8e714c6fe36c98
natasya aresta <putrianatasyaa09@gmail.com>
2024-04-02 15:32:44 +0700

    praktikum_06

--- praktikum_06/saving_app/lib/styles/text_style.dart
+++ praktikum_06/saving_app/lib/styles/text_style.dart
@@ -10,3 +10,38 @@ TextStyle kHeading6 = GoogleFonts.poppins(
   fontSize: 18,
   fontWeight: FontWeight.w700,
 );
+
+TextStyle kSubtitle1 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle kSubtitle2 = GoogleFonts.poppins(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle kBody1 = GoogleFonts.poppins(
+  fontSize: 13,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle kBody2 = GoogleFonts.poppins(
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle kButton1 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle kButton2 = GoogleFonts.poppins(
+  fontSize: 13,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle kCaption = GoogleFonts.poppins(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
