b3b5678446a82d3dbe3d0e3b3bf94a8015533083
natasya aresta <putrianatasyaa09@gmail.com>
2024-04-02 14:02:50 +0700

    praktikum_06

---
+++ praktikum_06/saving_app/lib/pages/on_boarding_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ praktikum_06/saving_app/lib/styles/text_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle kHeading5 = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle kHeading6 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+);
