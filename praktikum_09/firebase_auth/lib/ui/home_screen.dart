8db72082baaef43d6f4e48b5a608a1932d1ea4f2
natasyaap <21102118@ittelkom-pwt.ac.id>
2024-05-30 19:22:14 +0700

    praktikum_09

--- praktikum_09/firebase_auth/lib/ui/home_screen.dart
+++ praktikum_09/firebase_auth/lib/ui/home_screen.dart
@@ -1,38 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_project/ui/login.dart';
 import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
+import 'package:google_sign_in/google_sign_in.dart';
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            Text(
-              "HOME SCREEN",
-              style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff3D4DE0)),
-            ),
-            ElevatedButton( 
-              onPressed: () async { 
-                FirebaseAuth.instance 
-                .signOut() 
-                .then((value) => Navigator.pushAndRemoveUntil( 
-                  context, 
-                  MaterialPageRoute( builder: (context) => const LoginScreen(), 
-                  ), 
-                  (route) => false));
-              },
-              child: const Text('Keluar'), 
-            ), 
-          ], 
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Home Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              GoogleSignIn().signOut();
+              FirebaseAuth.instance
+                .signOut()
+                .then((value) => Navigator.pushAndRemoveUntil(
+                context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
+                  ),
+                      (route) => false));
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
