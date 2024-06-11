6b4660b113330e6e4d7de451c3d0317a975290d3
natasyaap <21102118@ittelkom-pwt.ac.id>
2024-06-11 20:52:22 +0700

    update home screen

--- praktikum_09/firebase_auth/lib/ui/home_screen.dart
+++ praktikum_09/firebase_auth/lib/ui/home_screen.dart
@@ -90,8 +90,88 @@ class _HomeScreenState extends State<HomeScreen> {
                                   child: PopupMenuButton<String>(
                                     onSelected: (value) {
                                       if (value == 'edit') {
+                                        showModalBottomSheet(
+                                            context: context,
+                                            isScrollControlled: true,
+                                            builder: (context) {
+                                              return Padding(
+                                                padding:
+                                                    const EdgeInsets.all(16.0),
+                                                child: Form(
+                                                  key: _formKey,
+                                                  child: Column(
+                                                    mainAxisSize:
+                                                        MainAxisSize.min,
+                                                    children: [
+                                                      TextFormField(
+                                                        controller: titleEdc,
+                                                      ),
+                                                      const SizedBox(
+                                                          height: 10.0),
+                                                      SizedBox(
+                                                          height: 300,
+                                                          child: TextFormField(
+                                                            controller: noteEdc,
+                                                            maxLines:
+                                                                null, // Set this
+                                                            expands:
+                                                                true, // and this
+                                                            keyboardType:
+                                                                TextInputType
+                                                                    .multiline,
+                                                          )),
+                                                      Padding(
+                                                          padding: EdgeInsets.only(
+                                                              bottom: MediaQuery
+                                                                      .of(
+                                                                          context)
+                                                                  .viewInsets
+                                                                  .bottom),
+                                                          child: SizedBox(
+                                                              width:
+                                                                  MediaQuery.of(
+                                                                          context)
+                                                                      .size
+                                                                      .width,
+                                                              child:
+                                                                  ElevatedButton(
+                                                                      onPressed:
+                                                                          () async {
+                                                                        if (_formKey
+                                                                            .currentState!
+                                                                            .validate()) {
+                                                                          try {
+                                                                            await _firestore.collection('tasks').doc(document.id).update({
+                                                                              'title': titleEdc.text,
+                                                                              'note': noteEdc.text,
+                                                                              'timestamp': FieldValue.serverTimestamp(),
+                                                                            });
+                                                                            ScaffoldMessenger.of(context).showSnackBar(
+                                                                              const SnackBar(content: Text('Note berhasil diperbarui')),
+                                                                            );
+                                                                            Navigator.pop(context);
+                                                                          } catch (e) {
+                                                                            ScaffoldMessenger.of(context).showSnackBar(
+                                                                              SnackBar(content: Text('$e')),
+                                                                            );
+                                                                          }
+                                                                        }
+                                                                      },
+                                                                      child: const Text(
+                                                                          'Save'))))
+                                                    ],
+                                                  ),
+                                                ),
+                                              );
+                                            });
+
                                         //tindakan edit
                                       } else if (value == 'delete') {
+                                        String documentId = document.id;
+                                        _firestore
+                                            .collection('tasks')
+                                            .doc(documentId)
+                                            .delete();
                                         //tindakan edit
                                       }
                                     },
